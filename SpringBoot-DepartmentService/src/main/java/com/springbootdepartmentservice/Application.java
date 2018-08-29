e1165798c68c35ba4d548cbac405d073ab310ce1
sandeep sharma <sandeep.sharma@trantorinc.com>
2018-08-29 17:56:58 +0530

    changes

--- SpringBoot-DepartmentService/src/main/java/com/springbootdepartmentservice/Application.java
+++ SpringBoot-DepartmentService/src/main/java/com/springbootdepartmentservice/Application.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import com.springbootdepartmentservice.model.Department;
+import com.springbootdepartmentservice.repository.DepartmentRepository;
 
 @EnableFeignClients
 @EnableDiscoveryClient
@@ -12,5 +16,15 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	DepartmentRepository repository() {
+		DepartmentRepository repository = new DepartmentRepository();
+		repository.add(new Department(1L, "Development"));
+		repository.add(new Department(1L, "Operations"));
+		repository.add(new Department(2L, "Development"));
+		repository.add(new Department(2L, "Operations"));		
+		return repository;
 	}
 }
--- SpringBoot-EmployeeService/src/main/java/com/springbootemployeeservice/Application.java
+++ SpringBoot-EmployeeService/src/main/java/com/springbootemployeeservice/Application.java
@@ -3,6 +3,10 @@ package com.springbootemployeeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import com.springbootemployeeservice.model.Employee;
+import com.springbootemployeeservice.repository.EmployeeRepository;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -10,5 +14,21 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	EmployeeRepository repository() {
+		EmployeeRepository repository = new EmployeeRepository();
+		repository.add(new Employee(1L, 1L, "John Smith", 34, "Analyst"));
+		repository.add(new Employee(1L, 1L, "Darren Hamilton", 37, "Manager"));
+		repository.add(new Employee(1L, 1L, "Tom Scott", 26, "Developer"));
+		repository.add(new Employee(1L, 2L, "Anna London", 39, "Analyst"));		
+		repository.add(new Employee(1L, 2L, "Patrick Dempsey", 27, "Developer"));
+		repository.add(new Employee(2L, 3L, "Kevin Price", 38, "Developer"));		
+		repository.add(new Employee(2L, 3L, "Ian Scott", 34, "Developer"));
+		repository.add(new Employee(2L, 3L, "Andrew Campton", 30, "Manager"));
+		repository.add(new Employee(2L, 4L, "Steve Franklin", 25, "Developer"));
+		repository.add(new Employee(2L, 4L, "Elisabeth Smith", 30, "Developer"));
+		return repository;
 	}
 }
