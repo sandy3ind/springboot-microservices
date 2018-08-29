b364f73938c4bbb8da04d57283e6564ab87c9ffd
sandeep sharma <sandeep.sharma@trantorinc.com>
2018-08-29 17:26:11 +0530

    first changes

---
+++ SpringBoot-OrganizationService/src/main/java/com/springbootorganizationservice/Application.java
@@ -0,0 +1,28 @@
+package com.springbootorganizationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import com.springbootorganizationservice.model.Organization;
+import com.springbootorganizationservice.repository.OrganizationRepository;
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	OrganizationRepository repository() {
+		OrganizationRepository repository = new OrganizationRepository();
+		repository.add(new Organization("Microsoft", "Redmond, Washington, USA"));
+		repository.add(new Organization("Oracle", "Redwood City, California, USA"));	
+		return repository;
+	}
+}
