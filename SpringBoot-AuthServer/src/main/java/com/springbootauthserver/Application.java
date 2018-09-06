a0428199f3f10024cb2e6c8db09dbe2adefb58cc
sandeep sharma <sandeep.sharma@trantorinc.com>
2018-09-06 16:23:33 +0530

    changes

---
+++ SpringBoot-AuthServer/src/main/java/com/springbootauthserver/Application.java
@@ -0,0 +1,24 @@
+package com.springbootauthserver;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+@SpringBootApplication
+@Import(AuthorizationServerConfig.class)
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}	
+	
+}
