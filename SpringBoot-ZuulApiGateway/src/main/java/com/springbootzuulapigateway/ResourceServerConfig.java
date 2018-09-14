c82c2ff192186266bce8137ad76da1a6bb295bd8
sandeep sharma <sandeep.sharma@trantorinc.com>
2018-09-14 11:46:43 +0530

    roles changes

--- SpringBoot-ZuulApiGateway/src/main/java/com/springbootzuulapigateway/ResourceServerConfig.java
+++ SpringBoot-ZuulApiGateway/src/main/java/com/springbootzuulapigateway/ResourceServerConfig.java
@@ -13,8 +13,16 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Override
     public void configure(HttpSecurity http) throws Exception {
         http           
-            .authorizeRequests().antMatchers("/api/organization/**")
+            .authorizeRequests()
+            .antMatchers("/api/organization/**")
             .hasAnyAuthority("ORGANIZATION_READ", "ORGANIZATION_CREATE", "ORGANIZATION_UPDATE", "ORGANIZATION_DELETE")
+            .antMatchers("/api/department/**")
+            .hasAnyAuthority("ORGANIZATION_READ", "ORGANIZATION_CREATE", "ORGANIZATION_UPDATE", "ORGANIZATION_DELETE",
+            		"DEPARTMENT_READ", "DEPARTMENT_CREATE", "DEPARTMENT_UPDATE", "DEPARTMENT_DELETE")
+            .antMatchers("/api/employee/**")
+            .hasAnyAuthority("ORGANIZATION_READ", "ORGANIZATION_CREATE", "ORGANIZATION_UPDATE", "ORGANIZATION_DELETE",
+            		"DEPARTMENT_READ", "DEPARTMENT_CREATE", "DEPARTMENT_UPDATE", "DEPARTMENT_DELETE",
+            		"EMPLOYEE_READ", "EMPLOYEE_CREATE", "EMPLOYEE_UPDATE", "EMPLOYEE_DELETE")
             .anyRequest().authenticated();
     }
 }
